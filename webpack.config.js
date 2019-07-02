aa5b2c493f5b9b34677d765fcf6a1d04fb5a8426
Geoffrey Willis Barugahare <benwill2014@gmail.com>
2019-07-02 14:20:26 +0300

    chore(setup): setting react router dom
    - ensure there standard http://localhost:8080/ leads to the home page
    - homepage has a link to the login page
    - ensure http://localhost:8080/login directs to the login page
    - ensure addition of a notfound page in case requested route not found
    
    [Finishes #164691174]

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'index-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
