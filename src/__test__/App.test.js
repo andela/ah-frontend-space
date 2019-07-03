c44b1cf4c862b441ee89bad674f895c5da161fc0
Geoffrey Willis Barugahare <benwill2014@gmail.com>
2019-07-03 11:19:20 +0300

    chore(codeclimate): run codeclimate test coverage
    - add a .circleci/config.yml file
    - configure circleci to run the tests and report the coverage
    - add badges for test coverage and maintainability to the
      README.md file
    
    [Finishes #166880072]

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,11 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from '../components/App';
-import React from 'react'
-import { shallow } from 'enzyme'
 
 
 describe('App', () => {
-    it('should match snapshot', () => {
-        const wrapper = shallow(<App />)
-        expect(wrapper.find('h1').text()).toBe('Welcome to Authors Haven')
-    })
-})
+  it('should match snapshot', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toBe('Welcome to Authors Haven');
+  });
+});
